f17fa6aab5640ec6f92101b29b64dbbac9fa1d28
melilocacr <melilocacr@gmail.com>
2018-04-20 04:43:25 -0700

    can output JSON data to console

--- js/product-info.js
+++ js/product-info.js
@@ -1,75 +1,64 @@
+var stock = {
+    "myShirt": [
+        {
+            "size_text": "Small",
+            "quantity": 0,
+        },
+        {
+            "size_text": "Medium",
+            "quantity": 0,
+        },
+        {
+            "size_text": "Large",
+            "quantity": 0,
+        },
+    ],
+
+    "myShirt2": [
+        {
+            "size_text": "Small",
+            "quantity": 3,
+        },
+        {
+            "size_text": "Medium",
+            "quantity": 0,
+        },
+        {
+            "size_text": "Large",
+            "quantity": 0,
+        },
+    ],
+    "myShirt3": [
+        {
+            "size_text": "Small",
+            "quantity": 3,
+        },
+        {
+            "size_text": "Medium",
+            "quantity": 0,
+        },
+        {
+            "size_text": "Large",
+            "quantity": 0,
+        },
+    ]
+};
+
 class Products extends React.Component {
     constructor() {
         super();
         this.state = {
-            data: [
-                {
-                    "myShirt": [
-                        {
-                            "size": "Small",
-                            "quantity": 0,
-                        },
-                        {
-                            "size_text": "Medium",
-                            "quantity": 0,
-                        },
-                        {
-                            "size_text": "Large",
-                            "quantity": 0,
-                        },
-                    ],
-
-                    "myShirt2": [
-                        {
-                            "size_text": "Small",
-                            "quantity": 3,
-                        },
-                        {
-                            "size_text": "Medium",
-                            "quantity": 0,
-                        },
-                        {
-                            "size_text": "Large",
-                            "quantity": 0,
-                        },
-                    ],
-                    "myShirt3": [
-                        {
-                            "size_text": "Small",
-                            "quantity": 3,
-                        },
-                        {
-                            "size_text": "Medium",
-                            "quantity": 0,
-                        },
-                        {
-                            "size_text": "Large",
-                            "quantity": 0,
-                        },
-                    ]
-                }]
+            data: stock
         }
     }
     render() {
         return (
             <div>
                 {
-                    // DISPLAY DESIRED OUTPUT HERE
-
-                    /**
-                      myShirt
-                        Small has 0 in stock
-                        Medium has 0 in stock
-                        Large has 0 in stock
-                      myShirt2
-                        Small has 0 in stock
-                        Medium has 0 in stock
-                        Large has 0 in stock
-                      myShirt3
-                        Small has 0 in stock
-                        Medium has 0 in stock
-                        Large has 0 in stock
-                    */
+                    Object.keys(stock).map(e => {
+                        console.log(e + ":");
+                        stock[e].map(o => console.log(o.size_text + " has " + o.quantity + " in stock"))
+                    })
                 }
             </div>
         );
