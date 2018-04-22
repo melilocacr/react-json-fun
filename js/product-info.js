87a43ec60ef066c234346e0204756b6b31eae375
melilocacr <melilocacr@gmail.com>
2018-04-22 16:17:36 -0700

    added navbar and form component

--- js/product-info.js
+++ js/product-info.js
@@ -44,31 +44,98 @@ var stock = { // Set this variable equal to the API response, after validating f
     ]
 };
 
-class Products extends React.Component {
+class RequestForm extends React.Component {
     constructor() {
         super();
+
         this.state = {
-            data: stock
+            productId: '',
+            fetchedProducts: {}
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.fetchProductData = this.fetchProductData.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({ productId: event.target.value });
+    }
+
+    handleSubmit(event) {
+        this.fetchProductData();
+        event.preventDefault();
+    }
+
+    fetchProductData() {
+        var sizeRequestUrl = 'https://www.dollskill.com/codetest/api.php?ids=' + this.state.productId + '&op=get_size_attributes';
+
+        fetch(sizeRequestUrl)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({ fetchedProducts: result })
+                    console.log(this.state.fetchedProducts);
+                }
+            );
+
+        
+    }
+
     render() {
         return (
-            <div id="product-container">{Object.keys(stock).map(productId => {
-            return <div id="product-info">
+            <div class="row">
+                <div class="col-md-3 sidepanel">
+                    <div class="form-container">
+                        <h3>Product Info Request</h3>
+                        <hr />
+                        <form onSubmit={this.handleSubmit}>
+                            <div class="form-group">
+                                <label htmlFor="product-id">Product Id(s):</label>
+                                <input type="text" class="form-control"
+                                    placeholder="Enter product Id(s) separated by commas (i.e 143249,142593,141975,150605)"
+                                    value={this.state.productId}
+                                    onChange={this.handleChange} />
+                            </div>
+                            <button type="submit" class="btn btn-default">SUBMIT</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <ProductList products={this.state.fetchedProducts} />
+                </div>
+            </div>
+        );
+    }
+}
+
+class ProductList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: {}
+        }
+    }
+
+    render() {
+        return (
+            <div id="product-container">{Object.keys(this.state.data).map(productId => {
+                return <div id="product-info">
                     <h2>{productId}</h2>
                     <div id="product-line-detail">{
-                        stock[productId].map(productDetail => <div className={'left'}>{`${productDetail.size_text} has ${productDetail.quantity} in stock`}</div>)
+                        this.state.data[productId].map(productDetail => <div className={'left'}>{`${productDetail.size_text} has ${productDetail.quantity} in stock`}</div>)
                     }
                     </div>
-            </div>
+                </div>
             })}
             </div>
         )
     }
-} 
+}
 
 ReactDOM.render(
-    <Products />,
-    document.getElementById('products'),
+    <RequestForm />,
+    document.getElementById('form'),
     function () { console.log('ReactDOM.render has been called'); }
 );
